09f81af0f9e2601702fa2e0695474774765d1706
aaronpowell <me@aaron-powell.com>
2011-02-03 14:24:12 +1100

    starting the ruby app

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source :gemcutter
+gem 'sinatra', '1.0'
+gem 'haml'
+gem 'couchrest', '>= 0.37'
+
+group :development do
+  gem 'sinatra-reloader'
+end
---
+++ site.rb
@@ -0,0 +1,23 @@
+['rubygems', 'sinatra', 'haml', 'pony', 'couchrest'].each {|gem| require gem}
+require 'sinatra/reloader' if development?
+
+if ENV['CLOUDANT_URL']
+  set :db, CouchRest.database!( ENV['CLOUDANT_URL'] + '/htteapot' )
+else
+  set :db, CouchRest.database!( 'http://localhost:5984/htteapot' )
+end
+
+helpers do
+  include Rack::Utils
+
+  def h(source)
+    escape_html(source).gsub(' ', '%20')
+  end
+
+end
+
+set :haml, :format => :html5
+
+get '/' do
+	haml :index
+end
