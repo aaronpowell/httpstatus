0ce6a12f7a516391cd6fbe8487ddaac6bed08ab8
Andrew Tobin <andrew@grrargh.com>
2011-02-21 14:00:11 +1100

    Added codes as a set of global hashes to the site - it should be loaded and kept in memory when the site starts and cached from there.
    Removed all references to CloudDb.
    
    Changed iterator in index to iterate over the values of the hashes.
    
    No more waiting on reads.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source :gemcutter
 gem 'sinatra', '1.0'
 gem 'haml'
-gem 'couchrest', '>= 0.37'
 
 group :development do
   gem 'sinatra-reloader'
--- site.rb
+++ site.rb
@@ -1,12 +1,6 @@
-['rubygems', 'sinatra', 'haml', 'couchrest'].each {|gem| require gem}
+['rubygems', 'sinatra', 'haml'].each {|gem| require gem}
 require 'sinatra/reloader' if development?
 require 'newrelic_rpm' if production?
-
-if ENV['CLOUDANT_URL']
-  set :db, CouchRest.database!( ENV['CLOUDANT_URL'] + '/htteapot' )
-else
-  set :db, CouchRest.database!( 'http://localhost:5984/htteapot' )
-end
 
 helpers do
   include Rack::Utils
@@ -19,12 +13,52 @@ end
 
 set :haml, :format => :html5
 
+$codes = { '200' => { 'status' => '200', 'description' => 'OK'  },
+           '201' => { 'status' => '201', 'description' => 'Created' },
+           '202' => { 'status' => '202', 'description' => 'Accepted' },
+           '203' => { 'status' => '203', 'description' => 'Non-Authoritative Information' },
+           '204' => { 'status' => '204', 'description' => 'No Content', 'exclude' => [ 'Content-Type', 'Content-Length'], 'excludeBody' => 'true' },
+           '205' => { 'status' => '205', 'description' => 'Reset Content', 'exclude' => [ 'Content-Type', 'Content-Length'], 'excludeBody' => 'true' },
+           '206' => { 'status' => '206',	'description' => 'Partial Content', 'headers' => { 'Content-Range' => '0-30' } },
+           '300' => { 'status' => '300', 'description' => 'Multiple Choices' },
+           '301' => { 'status' => '301', 'description' => 'Moved Permanently', 'headers' => { 'Location' => 'http://httpstat.us' } },
+           '302' => { 'status' => '302', 'description' => 'Found', 'headers' => { 'Location' => 'http://httpstat.us' } },
+           '303' => { 'status' => '303', 'description' => 'See Other', 'headers' => { 'Location' => 'http://httpstat.us' } },
+           '304' => { 'status' => '304',	'description' => 'Not Modified', 'exclude' => [ 'Content-Type', 'Content-Length'], 'excludeBody' => 'true' },
+           '305' => { 'status' => '305', 'description' => 'Use Proxy', 'headers' => { 'Location' => 'http://httpstat.us' } },
+           '306' => { 'status' => '306', 'description' => 'Unused' },
+           '307' => { 'status' => '307', 'description' => 'Temporary Redirect', 'headers' => { 'Location' => 'http://httpstat.us' } },
+           '400' => { 'status' => '400', 'description' => 'Bad Request' },
+           '401' => { 'status' => '401', 'description' => 'Unauthorized', 'headers' => { 'WWW-Authenticate' => 'Basic realm=\"Fake Realm\"' } },
+           '402' => { 'status' => '402', 'description' => 'Payment Required' },
+           '403' => { 'status' => '403', 'description' => 'Forbidden' },
+           '404' => { 'status' => '404', 'description' => 'Not Found' },
+           '405' => { 'status' => '405', 'description' => 'Method Not Allowed' },
+           '406' => { 'status' => '406', 'description' => 'Not Acceptable' },
+           '407' => { 'status' => '407', 'description' => 'Proxy Authentication Required', 'headers' => { 'Proxy-Authenticate' => 'Basic realm=\"Fake Realm\"' } },
+           '408' => { 'status' => '408', 'description' => 'Request Timeout' },
+           '409' => { 'status' => '409', 'description' => 'Conflict' },
+           '410' => { 'status' => '410', 'description' => 'Gone' },
+           '411' => { 'status' => '411', 'description' => 'Length Required' },
+           '412' => { 'status' => '412', 'description' => 'Precondition Required' },
+           '413' => { 'status' => '413', 'description' => 'Request Entity Too Large' },
+           '414' => { 'status' => '414', 'description' => 'Request-URI Too Long' },
+           '415' => { 'status' => '415', 'description' => 'Unsupported Media Type' },
+           '416' => { 'status' => '416', 'description' => 'Requested Range Not Satisfiable' },
+           '417' => { 'status' => '417', 'description' => 'Expectation Failed' },
+           '418' => { 'status' => '418', 'description' => 'I\'m a teapot', 'link' => 'http://www.ietf.org/rfc/rfc2324.txt' },
+           '419' => { 'status' => '500', 'description' => 'Internal Server Error' },
+           '501' => { 'status' => '501', 'description' => 'Not Implemented' },
+           '502' => { 'status' => '502', 'description' => 'Bad Gateway' },
+           '503' => { 'status' => '503', 'description' => 'Service Unavailable' },
+           '504' => { 'status' => '504', 'description' => 'Gateway Timeout' },
+           '505' => { 'status' => '505', 'description' => 'HTTP Version Not Supported' } }
+
 def processStatusCode()
 	code = params[:captures].first
-	statuses = options.db.view('status/by_status', :key => code)
-	if(statuses['rows'].length === 1)
-		status = statuses['rows'].first['value']
+	status = $codes[code]
 
+	unless(status.nil?)
 		headers = { }
 		if(status['headers'])
 			customHeaders = status['headers']
@@ -55,8 +89,7 @@ get '/im-a-teapot' do
 end
 
 get '/' do
-	statuses = options.db.view('status/by_status')
-	haml :index, :locals => {:statuses => statuses['rows']}
+		 haml :index, :locals => {:statuses => $codes}
 end
 
 not_found do
