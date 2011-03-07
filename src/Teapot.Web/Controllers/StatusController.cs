d92ec2a40ec405ded6c0bb1a3fdd0b87f0e7941b
Tatham Oddie <tatham@oddie.com.au>
2011-03-07 23:29:45 +1100

    Renamed field for ReSharper happiness.

--- src/Teapot.Web/Controllers/StatusController.cs
+++ src/Teapot.Web/Controllers/StatusController.cs
@@ -5,17 +5,17 @@ namespace Teapot.Web.Controllers
 {
     public class StatusController : Controller
     {
-        private static readonly StatusCodeResults _statusCodes = new StatusCodeResults();
+        static readonly StatusCodeResults statusCodes = new StatusCodeResults();
 
         public ActionResult Index()
         {
-            return View(_statusCodes);
+            return View(statusCodes);
         }
 
         public ActionResult StatusCode(int statusCode)
         {
-            var status = _statusCodes.ContainsKey(statusCode)
-                ? _statusCodes[statusCode]
+            var status = statusCodes.ContainsKey(statusCode)
+                ? statusCodes[statusCode]
                 : new StatusCodeResult {Description = string.Format("{0} Unknown Code", statusCode)};
 
             return new HttpStatusCodeResult(statusCode, status.Description);
