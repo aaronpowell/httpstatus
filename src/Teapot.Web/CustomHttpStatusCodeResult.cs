505968054a6c601c5ee01a0893498c1fa66a884b
aaronpowell <me@aaron-powell.com>
2011-03-07 23:56:21 +1100

    setting up the base implementation of the custom action result. Also migrated to IIS express to better deal with headers

--- src/Teapot.Web/CustomHttpStatusCodeResult.cs
+++ src/Teapot.Web/CustomHttpStatusCodeResult.cs
@@ -5,9 +5,30 @@ namespace Teapot.Web
 {
     public class CustomHttpStatusCodeResult : HttpStatusCodeResult
     {
+        private readonly StatusCodeResult _statusData;
+
         public CustomHttpStatusCodeResult(int statusCode, StatusCodeResult statusData)
             : base(statusCode, statusData.Description)
         {
+            _statusData = statusData;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (_statusData.ExcludeBody)
+            {
+                if (!string.IsNullOrEmpty(context.HttpContext.Response.Headers["Content-Length"]))
+                    context.HttpContext.Response.Headers.Remove("Content-Length");
+
+                if (!string.IsNullOrEmpty(context.HttpContext.Response.Headers["Content-Type"]))
+                    context.HttpContext.Response.Headers.Remove("Content-Type");
+            }
+
+            if (_statusData.IncludeHeaders != null)
+                foreach (var header in _statusData.IncludeHeaders)
+                    context.HttpContext.Response.Headers.Add(header.Key, header.Value);
+
+            base.ExecuteResult(context);
         }
     }
 }
