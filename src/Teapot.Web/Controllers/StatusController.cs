c5f85f3c25ace8d9b59ab00ae362e1e5fc102fba
Toby Moore <tmo@trustpilot.com>
2017-06-08 00:22:54 +0200

    Add CORS route that return 200 on OPTIONS calls (#29)
    * Remove non-compiling code
    
    * Use an attribute to set CORS headers rather than web.config
    
    * Add cors route that returns 200 with access control headers for OPTIONS requests
    
    * Move cors attribute to own file

--- src/Teapot.Web/Controllers/StatusController.cs
+++ src/Teapot.Web/Controllers/StatusController.cs
@@ -1,8 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Web.Mvc;
 using Teapot.Web.Models;
 
 namespace Teapot.Web.Controllers
 {
+    [CorsAllowAnyOrigin]
     public class StatusController : Controller
     {
         static readonly StatusCodeResults StatusCodes = new StatusCodeResults();
@@ -19,6 +22,28 @@ namespace Teapot.Web.Controllers
                 : new StatusCodeResult {Description = string.Format("{0} Unknown Code", statusCode)};
 
             return new CustomHttpStatusCodeResult(statusCode, statusData);
+        }
+
+        public ActionResult Cors(int statusCode)
+        {
+            if (Request.HttpMethod != "OPTIONS")
+            {
+                return StatusCode(statusCode);
+            }
+
+            var allowedOrigin = Request.Headers.Get("Origin") ?? "*";
+            var allowedMethod = Request.Headers.Get("Access-Control-Request-Method") ?? "GET";
+            var allowedHeaders = Request.Headers.Get("Access-Control-Request-Headers") ?? "X-Anything";
+
+            var responseHeaders = new Dictionary<string, string>
+            {
+                { "Access-Control-Allow-Origin", allowedOrigin },
+                { "Access-Control-Allow-Headers", allowedHeaders },
+                { "Access-Control-Allow-Methods", allowedMethod }
+            };
+
+            var statusData = new StatusCodeResult { IncludeHeaders = responseHeaders };
+            return new CustomHttpStatusCodeResult((int)HttpStatusCode.OK, statusData);
         }
     }
 }
