bd0b2bb2e5c5947de2a38ecbdc20fbdd7fd45759
balarajagopal <bala.cis@gmail.com>
2017-06-12 19:48:19 -0400

    Adding optional parameter "sleep" with min and max limits (#31)
    * Adding an optional parameter "sleep" 
    
    Adding an optional parameter "sleep" to delay response
    
    * Adding optional parameter "sleep" with min and max limits
    
    Adding optional parameter "sleep" with min and max limits of 0 and 300000 (5 mins).
    
    E.g. usage:
    http://server-name/400?sleep=5000
    This will return HTTP 400 after 5 seconds 
    
    http://server-name/400
    This will return HTTP 400 immediatly
    
    * Adding optional parameter "sleep" with min and max limits
    
    Adding optional parameter "sleep" with min and max limits of 0 and 300000 (5 mins).
    
    E.g. usage:
    http://server-name/400?sleep=5000
    This will return HTTP 400 after 5 seconds 
    
    http://server-name/400
    This will return HTTP 400 immediatly
    
    * Adding helper method to avoid code repetition
    
    Adding helper method DoSleep to avoid some code repetition.
    
    * Using const for constants instead of static readonly
    
    Using const for constants instead of static readonly so they can be used to assign optional sleep paramater

--- src/Teapot.Web/Controllers/StatusController.cs
+++ src/Teapot.Web/Controllers/StatusController.cs
@@ -10,25 +10,30 @@ namespace Teapot.Web.Controllers
     {
         static readonly StatusCodeResults StatusCodes = new StatusCodeResults();
 
+        private const int SLEEP_MIN = 0;
+        private const int SLEEP_MAX = 300000; // 5 mins in milliseconds
+
         public ActionResult Index()
         {
             return View(StatusCodes);
         }
 
-        public ActionResult StatusCode(int statusCode)
+        public ActionResult StatusCode(int statusCode, int? sleep = SLEEP_MIN)
         {
             var statusData = StatusCodes.ContainsKey(statusCode)
                 ? StatusCodes[statusCode]
-                : new StatusCodeResult {Description = string.Format("{0} Unknown Code", statusCode)};
+                : new StatusCodeResult { Description = string.Format("{0} Unknown Code", statusCode) };
+
+            DoSleep(sleep);
 
             return new CustomHttpStatusCodeResult(statusCode, statusData);
         }
 
-        public ActionResult Cors(int statusCode)
+        public ActionResult Cors(int statusCode, int? sleep = SLEEP_MIN)
         {
             if (Request.HttpMethod != "OPTIONS")
             {
-                return StatusCode(statusCode);
+                return StatusCode(statusCode, sleep);
             }
 
             var allowedOrigin = Request.Headers.Get("Origin") ?? "*";
@@ -43,7 +48,39 @@ namespace Teapot.Web.Controllers
             };
 
             var statusData = new StatusCodeResult { IncludeHeaders = responseHeaders };
+
+            DoSleep(sleep);
+
             return new CustomHttpStatusCodeResult((int)HttpStatusCode.OK, statusData);
+        }
+
+        private static void DoSleep(int? sleep)
+        {
+            int sleepData = SanitizeSleepParameter(sleep, SLEEP_MIN, SLEEP_MAX);
+
+            if (sleepData > 0)
+            {
+                System.Threading.Thread.Sleep(sleepData);
+            }
+        }
+
+        private static int SanitizeSleepParameter(int? sleep, int min, int max)
+        {
+            var sleepData = sleep ?? 0;
+
+            // range check - minimum should be 0
+            if (sleepData < min)
+            {
+                sleepData = min;
+            }
+
+            // range check- maximum should be 300000 (5 mins)
+            if (sleepData > max)
+            {
+                sleepData = max;
+            }
+
+            return sleepData;
         }
     }
 }
