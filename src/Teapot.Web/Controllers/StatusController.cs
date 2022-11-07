f79e4cc5ea5f4a7c5ac4245345e6d2aba00818a6
Aaron Powell <me@aaron-powell.com>
2022-11-07 11:06:00 +1100

    adding 504 back in as it seems to not be getting abused anymore

--- src/Teapot.Web/Controllers/StatusController.cs
+++ src/Teapot.Web/Controllers/StatusController.cs
@@ -17,9 +17,6 @@ public class StatusController : Controller
     [Route("")]
     public IActionResult Index() => View(_statusCodes);
 
-    [HttpGet("504", Name = "Noop-Bypass")]
-    public IActionResult Noop() => new EmptyResult();
-
     [Route("{statusCode:int}", Name = "StatusCode")]
     [Route("{statusCode:int}/{*wildcard}", Name = "StatusCodeWildcard")]
     public async Task<IActionResult> StatusCode(int statusCode, int? sleep)
