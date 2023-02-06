b3b6e7186e9340a7971aff7d48ed65e6f9a3cd03
Aaron Powell <me@aaron-powell.com>
2023-02-06 12:11:56 +1100

    Adding sleep via X-Sleep header
    Also some test cleanup and moved sleep processing into ActionResult rather than Controller

---
+++ src/Teapot.Web.Tests/UnitTests/SleepTests.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Teapot.Web.Controllers;
+using Teapot.Web.Models;
+using Teapot.Web.Models.Unofficial;
+
+namespace Teapot.Web.Tests.UnitTests;
+internal class SleepTests {
+    private const int Sleep = 500;
+
+    private TeapotStatusCodeResults _statusCodes;
+
+    [SetUp]
+    public void Setup() {
+        _statusCodes = new(
+            new AmazonStatusCodeResults(),
+            new CloudflareStatusCodeResults(),
+            new EsriStatusCodeResults(),
+            new LaravelStatusCodeResults(),
+            new MicrosoftStatusCodeResults(),
+            new NginxStatusCodeResults(),
+            new TwitterStatusCodeResults()
+        );
+    }
+
+    [Test]
+    public void SleepReadFromQuery() {
+        StatusController controller = new(_statusCodes);
+
+        IActionResult result = controller.StatusCode(200, Sleep);
+
+        Assert.Multiple(() => {
+            Assert.That(result, Is.InstanceOf<CustomHttpStatusCodeResult>());
+
+            CustomHttpStatusCodeResult r = (CustomHttpStatusCodeResult)result;
+            Assert.That(r.Sleep, Is.EqualTo(Sleep));
+        });
+    }
+
+    [Test]
+    public void SleepReadFromHeader() {
+        StatusController controller = new(_statusCodes) {
+            ControllerContext = new ControllerContext {
+                HttpContext = new DefaultHttpContext()
+            }
+        };
+        controller.ControllerContext.HttpContext.Request.Headers.Add("X-Sleep", Sleep.ToString());
+
+        IActionResult result = controller.StatusCode(200, null);
+
+        Assert.Multiple(() => {
+            Assert.That(result, Is.InstanceOf<CustomHttpStatusCodeResult>());
+
+            CustomHttpStatusCodeResult r = (CustomHttpStatusCodeResult)result;
+            Assert.That(r.Sleep, Is.EqualTo(Sleep));
+        });
+    }
+
+    [Test]
+    public void SleepReadFromQSTakesPriorityHeader() {
+        StatusController controller = new(_statusCodes) {
+            ControllerContext = new ControllerContext {
+                HttpContext = new DefaultHttpContext()
+            }
+        };
+        controller.ControllerContext.HttpContext.Request.Headers.Add("X-Sleep", Sleep.ToString());
+
+        IActionResult result = controller.StatusCode(200, Sleep * 2);
+
+        Assert.Multiple(() => {
+            Assert.That(result, Is.InstanceOf<CustomHttpStatusCodeResult>());
+
+            CustomHttpStatusCodeResult r = (CustomHttpStatusCodeResult)result;
+            Assert.That(r.Sleep, Is.EqualTo(Sleep * 2));
+        });
+    }
+
+    [Test]
+    public void BadSleepHeaderIgnored() {
+        StatusController controller = new(_statusCodes) {
+            ControllerContext = new ControllerContext {
+                HttpContext = new DefaultHttpContext()
+            }
+        };
+        controller.ControllerContext.HttpContext.Request.Headers.Add("X-Sleep", "invalid");
+
+        IActionResult result = controller.StatusCode(200, null);
+
+        Assert.Multiple(() => {
+            Assert.That(result, Is.InstanceOf<CustomHttpStatusCodeResult>());
+
+            CustomHttpStatusCodeResult r = (CustomHttpStatusCodeResult)result;
+            Assert.That(r.Sleep, Is.Null);
+        });
+    }
+}
