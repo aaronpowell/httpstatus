95d7b5e6c89f48125072ed78858317034f6d5a22
Aaron Powell <me@aaron-powell.com>
2024-11-29 11:57:55 +1100

    case insensitive headers (#147)
    * Updating NuGet packages
    
    * Fixing #144

--- src/Teapot.Web.Tests/IntegrationTests/CustomHeaderTests.cs
+++ src/Teapot.Web.Tests/IntegrationTests/CustomHeaderTests.cs
@@ -43,6 +43,29 @@ public class CustomHeaderTests
     }
 
     [Test]
+    public async Task CanSetCustomHeadersCaseInsensitive()
+    {
+        string uri = "/200";
+        string headerName = "Foo";
+        string headerValue = "bar";
+
+        using HttpRequestMessage request = new(HttpMethod.Get, uri);
+        request.Headers.Add($"{StatusExtensions.CUSTOM_RESPONSE_HEADER_PREFIX.ToLower()}{headerName}", headerValue);
+
+        using HttpResponseMessage response = await _httpClient.SendAsync(request);
+
+        System.Net.Http.Headers.HttpResponseHeaders headers = response.Headers;
+        Assert.Multiple(() =>
+        {
+            Assert.That(headers.Contains(headerName), Is.True);
+            Assert.That(headers.TryGetValues(headerName, out IEnumerable<string>? values), Is.True);
+            Assert.That(values, Is.Not.Null);
+            Assert.That(values!.Count(), Is.EqualTo(1));
+            Assert.That(values!.First(), Is.EqualTo(headerValue));
+        });
+    }
+
+    [Test]
     public async Task Redirects302ToCorrectLocation()
     {
         string uri = "/302";
